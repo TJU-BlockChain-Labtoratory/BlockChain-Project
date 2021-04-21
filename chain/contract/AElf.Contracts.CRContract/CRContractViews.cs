db1b2f17e043903b77cc9e98dfba0cc3851257a9
GoldenSwordClock <1159561041@qq.com>
2021-04-21 15:15:01 +0800

    完成getCRTID()
    get ID of a CRT by its owner and content

--- chain/contract/AElf.Contracts.CRContract/CRContractViews.cs
+++ chain/contract/AElf.Contracts.CRContract/CRContractViews.cs
@@ -1,5 +1,6 @@
 ﻿using AElf.Types;
 using Google.Protobuf.WellKnownTypes;
+using AElf.Sdk.CSharp.State;
 
 namespace AElf.Contracts.CRContract
 {
@@ -44,6 +45,20 @@ namespace AElf.Contracts.CRContract
         public override CRT_List getAllCRTs(Address input)
         {
             return State.CRT_Account[input];
+        }
+
+        public override Hash getCRTID( GetCRTIDInput input )
+        {
+            var list = State.CRT_Account[input.Owner];
+            foreach ( Hash one in list.CRTSet )
+            {
+                var oneCRT = State.CRT_Base[one];
+                if ( oneCRT.Info.CRTContent == input.CRTContent )
+                {
+                    return one;
+                }
+            }
+            return new Hash(); //????如果错误，返回什么???
         }
     }
 }
